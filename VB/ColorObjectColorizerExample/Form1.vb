f22dc18a78a46238fc6003ef4013e34ffb6e718e
DevExpressExampleBot <fakeEmail>
2023-12-12 15:38:32 +0400

    Source auto update [skip ci]

--- VB/ColorObjectColorizerExample/Form1.vb
+++ VB/ColorObjectColorizerExample/Form1.vb
@@ -1,66 +1,56 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows.Forms
 Imports System.Xml.Linq
 Imports DevExpress.XtraCharts
 
 Namespace ColorObjectColorizerExample
-	Partial Public Class Form1
-		Inherits Form
 
-		Private Const filepath As String = "..\..\Data\GDP.xml"
+    Public Partial Class Form1
+        Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Const filepath As String = "..\..\Data\GDP.xml"
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-'			#Region "#BarSeries"
-			' Create and customize a bar series.
-			Dim barSeries As New Series() With {
-				.DataSource = LoadData(filepath),
-				.ArgumentDataMember = "Country",
-				.ColorDataMember = "NationalColor",
-				.View = New SideBySideBarSeriesView()
-			}
-			barSeries.View.Colorizer = New ColorObjectColorizer()
-			barSeries.ValueDataMembers.AddRange(New String() { "Product" })
-'			#End Region ' #BarSeries
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-			' Add the series to the ChartControl's Series collection.
-			chartControl1.Series.Add(barSeries)
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+#Region "#BarSeries"
+            ' Create and customize a bar series.
+            Dim barSeries As Series = New Series() With {.DataSource = LoadData(filepath), .ArgumentDataMember = "Country", .ColorDataMember = "NationalColor", .View = New SideBySideBarSeriesView()}
+            barSeries.View.Colorizer = New ColorObjectColorizer()
+            barSeries.ValueDataMembers.AddRange(New String() {"Product"})
+#End Region  ' #BarSeries
+            ' Add the series to the ChartControl's Series collection.
+            chartControl1.Series.Add(barSeries)
+            ' Show a title for the values axis.
+            CType(chartControl1.Diagram, XYDiagram).AxisY.Title.Text = "GDP per capita, $"
+            CType(chartControl1.Diagram, XYDiagram).AxisY.Title.Visibility = DevExpress.Utils.DefaultBoolean.True
+        End Sub
 
-			' Show a title for the values axis.
-			CType(chartControl1.Diagram, XYDiagram).AxisY.Title.Text = "GDP per capita, $"
-			CType(chartControl1.Diagram, XYDiagram).AxisY.Title.Visibility = DevExpress.Utils.DefaultBoolean.True
-		End Sub
+#Region "#DataLoad"
+        Private Class HpiPoint
 
-		#Region "#DataLoad"
-		Private Class HpiPoint
-			Public Property Country() As String
-			Public Property Product() As Double
-			Public Property NationalColor() As String
-		End Class
+            Public Property Country As String
 
-		' Loads data from an XML data source.
-		Private Shared Function LoadData(ByVal filepath As String) As List(Of HpiPoint)
-			Dim doc As XDocument = XDocument.Load(filepath)
-			Dim points As New List(Of HpiPoint)()
-			For Each element As XElement In doc.Element("G20HPIs").Elements("CountryStatistics")
-				points.Add(New HpiPoint() With {
-					.Country = element.Element("Country").Value,
-					.Product = Convert.ToDouble(element.Element("Product").Value),
-					.NationalColor = element.Element("NationalColor").Value
-				})
-			Next element
-			Return points
-		End Function
-		#End Region ' #DataLoad
-	End Class
+            Public Property Product As Double
+
+            Public Property NationalColor As String
+        End Class
+
+        ' Loads data from an XML data source.
+        Private Shared Function LoadData(ByVal filepath As String) As List(Of HpiPoint)
+            Dim doc As XDocument = XDocument.Load(filepath)
+            Dim points As List(Of HpiPoint) = New List(Of HpiPoint)()
+            For Each element As XElement In doc.Element("G20HPIs").Elements("CountryStatistics")
+                points.Add(New HpiPoint() With {.Country = element.Element("Country").Value, .Product = Convert.ToDouble(element.Element("Product").Value), .NationalColor = element.Element("NationalColor").Value})
+            Next
+
+            Return points
+        End Function
+#End Region  ' #DataLoad
+    End Class
 End Namespace
