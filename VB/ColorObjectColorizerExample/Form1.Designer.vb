b09134eb9430c99005817fd11d8289b1f0d06b04
DevExpressExampleBot <fakeEmail>
2023-12-12 15:37:17 +0400

    Source auto update [skip ci]

--- VB/ColorObjectColorizerExample/Form1.Designer.vb
+++ VB/ColorObjectColorizerExample/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace ColorObjectColorizerExample
-    Partial Public Class Form1
+Namespace ColorObjectColorizerExample
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,22 +12,22 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+'#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
-            Dim chartTitle2 As New DevExpress.XtraCharts.ChartTitle()
+            Dim chartTitle2 As DevExpress.XtraCharts.ChartTitle = New DevExpress.XtraCharts.ChartTitle()
             Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
-            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
             ' chartControl1
@@ -33,11 +35,11 @@
             Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
             Me.chartControl1.Location = New System.Drawing.Point(0, 0)
             Me.chartControl1.Name = "chartControl1"
-            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(){}
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(-1) {}
             Me.chartControl1.Size = New System.Drawing.Size(1264, 681)
             Me.chartControl1.TabIndex = 0
             chartTitle2.Text = "GDP for G20"
-            Me.chartControl1.Titles.AddRange(New DevExpress.XtraCharts.ChartTitle() { chartTitle2})
+            Me.chartControl1.Titles.AddRange(New DevExpress.XtraCharts.ChartTitle() {chartTitle2})
             ' 
             ' Form1
             ' 
@@ -47,14 +49,12 @@
             Me.Controls.Add(Me.chartControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
-
+'#End Region
         Private chartControl1 As DevExpress.XtraCharts.ChartControl
     End Class
 End Namespace
-
--- VB/ColorObjectColorizerExample/Form1.vb
+++ VB/ColorObjectColorizerExample/Form1.vb
@@ -1,56 +1,55 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows.Forms
 Imports System.Xml.Linq
 Imports DevExpress.XtraCharts
 
 Namespace ColorObjectColorizerExample
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
-        Private Const filepath As String = "..\..\Data\GDP.xml"
+        Const filepath As String = "..\..\Data\GDP.xml"
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-'            #Region "#BarSeries"
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+'#Region "#BarSeries"
             ' Create and customize a bar series.
-            Dim barSeries As New Series() With {.DataSource = LoadData(filepath), .Colorizer = New ColorObjectColorizer(), .ArgumentDataMember = "Country", .ColorDataMember = "NationalColor", .View = New SideBySideBarSeriesView()}
-            barSeries.ValueDataMembers.AddRange(New String() { "Product" })
-'            #End Region ' #BarSeries
-
+            Dim barSeries As Series = New Series() With {.DataSource = LoadData(filepath), .Colorizer = New ColorObjectColorizer(), .ArgumentDataMember = "Country", .ColorDataMember = "NationalColor", .View = New SideBySideBarSeriesView()}
+            barSeries.ValueDataMembers.AddRange(New String() {"Product"})
+'#End Region  ' #BarSeries
             ' Add the series to the ChartControl's Series collection.
             chartControl1.Series.Add(barSeries)
-
             ' Show a title for the values axis.
             CType(chartControl1.Diagram, XYDiagram).AxisY.Title.Text = "GDP per capita, $"
             CType(chartControl1.Diagram, XYDiagram).AxisY.Title.Visibility = DevExpress.Utils.DefaultBoolean.True
         End Sub
 
-        #Region "#DataLoad"
+'#Region "#DataLoad"
         Private Class HpiPoint
-            Public Property Country() As String
-            Public Property Product() As Double
-            Public Property NationalColor() As String
+
+            Public Property Country As String
+
+            Public Property Product As Double
+
+            Public Property NationalColor As String
         End Class
 
         ' Loads data from an XML data source.
         Private Shared Function LoadData(ByVal filepath As String) As List(Of HpiPoint)
             Dim doc As XDocument = XDocument.Load(filepath)
-            Dim points As New List(Of HpiPoint)()
+            Dim points As List(Of HpiPoint) = New List(Of HpiPoint)()
             For Each element As XElement In doc.Element("G20HPIs").Elements("CountryStatistics")
                 points.Add(New HpiPoint() With {.Country = element.Element("Country").Value, .Product = Convert.ToDouble(element.Element("Product").Value), .NationalColor = element.Element("NationalColor").Value})
-            Next element
+            Next
+
             Return points
         End Function
-        #End Region ' #DataLoad
+'#End Region  ' #DataLoad
     End Class
 End Namespace
